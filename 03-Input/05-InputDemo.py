09f0a461fdfa998f038ed898d8364cacd04ac584
Laxmikant Sanjay Tate <150119297+Laxmikant-2001@users.noreply.github.com>
2026-01-16 22:42:56 +0530

    Add files via upload

---
+++ 03-Input/05-InputDemo.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root_window = Tk()
+root_window.title("LST: Input-Demo")
+
+L = Label(root_window)
+L.configure(text="Tell me about your github")
+L.grid(row=0, column=0)
+
+E = Entry(root_window)
+E.grid(row=0, column=1)
+
+root_window.mainloop()
